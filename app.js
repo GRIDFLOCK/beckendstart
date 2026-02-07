17a13fe9f49ddaf7d84658f6babbe41b91b51b8c
grid <cleshakaunt970@gmail.com>
2026-02-07 17:51:45 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,53 @@
-const express = require("express");
-const fs = require("fs");
-const cors = require("cors");
-const { use } = require("react");
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(cors()); 
-app.use(express.json());
+app.use(cors({
+    origin: "https://github.com/GRIDFLOCK/singup.git"
+}));
+
+app.use(bodyParser.json());
+
+const users = [];
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function isValidPassword(password) {
+    const passwordRegex =  /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+    return passwordRegex.test(password);
+}
 
 app.post("/register", (req, res) => {
-    const{ login, password } = req.body;
-
-    if(!login || !password) {
-        return res.json({ success: false, message: "тут пусто"});
+    const { email, password} = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ success: false, message: "Всі поля обов'язкові!"})
     }
-
-    const users = JSON.parse(fs.readFileSync("users.json", "utf-8"));
-
-    const exists = users.find(u => u.login === login);
-    if (exists) {
-        return res.json({ success: false, message: "Користувач існує"});
+    if (!isValidEmail(email)) {
+        return res.status(400).json({ success: false, message: "Невірна адреса електронної пошти!"})
     }
+    if (!isValidPassword(password)) {
+        return res.status(400).json({ success: false, message: "Пароль повинен містити мінімум 6 символів, принаймні одну літеру та одну цифру!"})
+    }
+    if (users.find(u => u.email === email)) {
+        return res.status(400).json({ success: false, message: "Користувач з таким email вже існує!"})
+    }
+    users.push({ email, password});
 
-    users.push({ login, password });
-    fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
+    console.log("Registered users:", email);
 
-    res.json({ success: true, message: "Реєстрація успішна"}); 
+    res.json({ success: true, message: "Користувача успішно зареєстровано!"});
+
+});
+app.get("/", (req, res) => {
+    res.send("Server is running");
 });
 
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
