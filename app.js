ac43d37c98e1222d03c1b89f47c1413147388219
grid <cleshakaunt970@gmail.com>
2026-03-01 21:45:20 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -68,6 +68,19 @@ app.post("/register", (req, res) => {
     res.json({ success: true, message: "Користувача успішно зареєстровано!"});
     
 });
+
+app.post("/login", (req, res) => {
+    const { email, password} = req.body;
+    const user = users.find(u => u.email === email && u.password === password);
+
+    if (!user) {
+        return res.status(400).json({ 
+            success: false,
+            message: "Невірний email або пароль!"
+        })
+    }
+    res.json({ success: true, message: "Успішний вхід!", user });
+});
 app.get("/", (req, res) => {
     res.send("Server is running");
 });
